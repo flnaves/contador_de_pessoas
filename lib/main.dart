e4f656075cfe48e7a203725be36b85f5b1348e8a
Flavio <flavio.naves85@gmail.com>
2021-01-09 00:04:16 -0300

    Add image to background

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,57 @@ import 'package:flutter/Material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Contador de Pessoas",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          "Pessoas 0",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        Text(
-          "Pode Entrar!",
-          style: TextStyle(color: Colors.white, fontSize: 30 ,fontStyle: FontStyle.italic),
-        ),
-      ],
-    ),
-  ));
+      title: "Contador de Pessoas",
+      home: Stack(
+        children: <Widget>[
+          Image.asset(
+            "images/restaurante.jpg",
+            fit: BoxFit.cover,
+            height: 1000,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Pessoas 0",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text("+1",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold)),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text("-1",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold)),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ]),
+              Text(
+                "Pode Entrar!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontStyle: FontStyle.italic),
+              ),
+            ],
+          ),
+        ],
+      )));
 }
